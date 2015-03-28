1a98f34829858360a15ed2a1b2b5934a345cdcf4
Bruce <chiefengineer@azwebengineering.com>
2015-03-28 01:33:38 -0700

    New script.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 ﻿namespace Web.Controllers
 {
+	using System;
+	using System.Collections.Generic;
 	using System.Web.Mvc;
+	using FbcBookIt.Entity;
+	using FbcBookIt.Repository;
 
 	public class HomeController: Controller
 	{
-		public HomeController()
+		private readonly IBookLoanR _BookLoanR;
+
+		public HomeController(IBookLoanR aBookLoanR)
 		{
-			
+			if (aBookLoanR == null)
+			{
+				throw new ArgumentNullException("aBookLoanR");
+			}
+			_BookLoanR = aBookLoanR;
 		}
+
 		public ActionResult Index()
 		{
+			List<BookLoan> vList = _BookLoanR.GetAll();
 			return View();
 		}
 
