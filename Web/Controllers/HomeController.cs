7804361308c382a4eea7bcb0520631fa166f0cd0
Gary Pratt <gpratt5@gmail.com>
2015-03-28 02:23:18 -0700

    d

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,42 +1,43 @@
 ﻿namespace Web.Controllers
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Web.Mvc;
-	using FbcBookIt.Entity;
-	using FbcBookIt.Repository;
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using FbcBookIt.Entity;
+    using FbcBookIt.Repository;
 
-	public class HomeController: Controller
-	{
-		private readonly IBookLoanR _BookLoanR;
+    public class HomeController: Controller
+    {
+        private readonly IBookLoanR _BookLoanR;
 
-		public HomeController(IBookLoanR aBookLoanR)
-		{
-			if (aBookLoanR == null)
-			{
-				throw new ArgumentNullException("aBookLoanR");
-			}
-			_BookLoanR = aBookLoanR;
-		}
+        public HomeController(IBookLoanR aBookLoanR)
+        {
+            if (aBookLoanR == null)
+            {
+                throw new ArgumentNullException("aBookLoanR");
+            }
+            _BookLoanR = aBookLoanR;
+        }
 
-		public ActionResult Index()
-		{
-			List<BookLoan> vList = _BookLoanR.GetAll();
-			return View();
-		}
+        public ActionResult Index()
+        {
+            List<BookLoan> vList = _BookLoanR.GetAll();
+            
+            return View();
+        }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
