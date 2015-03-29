feb0df55e4acac583680719d017b75d9347fe755
Steve Hiner <stevehiner@gmail.com>
2015-03-28 21:18:57 -0700

    Updated for new fields.
    Better drilldown support for titles.

---
+++ BookItAdmin/Controllers/CopyController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FbcBookIt.Repository;
+using FbcBookIt.Entity;
+
+namespace BookItAdmin.Controllers
+{
+    public partial class CopyController : Controller
+    {
+        private ITitleR _titleR = null;
+        private ICopyR _copyR = null;
+        private IVolumeR _volumeR = null;
+        public CopyController(ITitleR titleR, ICopyR copyR, IVolumeR volumeR)
+        {
+            if (titleR == null)
+                throw new ArgumentNullException("Title repository cannot be null");
+            if (copyR == null)
+                throw new ArgumentNullException("Copy repository cannot be null");
+            if (volumeR == null)
+                throw new ArgumentNullException("Volume repository cannot be null");
+
+            _titleR = titleR;
+            _copyR = copyR;
+            _volumeR = volumeR;
+        }
+
+        public virtual ActionResult Index(Guid id)
+        {
+            var copy = _copyR.Get(id);
+            copy.Volumes = _volumeR.GetByCopyIDAsList(id);
+
+            return View(copy);
+        }
+
+        private void LoadParentStuff(Guid titleId)
+        {
+            var title = _titleR.Get(titleId);
+            ViewBag.TitleName = title.Name;
+            ViewBag.TitleId = title.TitleID;
+        }
+
+        public virtual ActionResult Details(Guid id)
+        {
+            var copy = _copyR.Get(id);
+            LoadParentStuff(copy.TitleID);
+
+            return View(copy);
+        }
+
+        public virtual ActionResult Create(Guid titleId)
+        {
+            var copy = new Copy();
+            copy.TitleID = titleId;
+            LoadParentStuff(titleId);
+
+            return View(copy);
+        }
+
+        [HttpPost]
+        public virtual ActionResult Create(Copy copy)
+        {
+            try
+            {
+                var id = _copyR.InsertAndReturnPrimaryKey(copy);
+
+                return RedirectToAction(MVC.Copy.Index());
+            }
+            catch
+            {
+                return View(copy);
+            }
+        }
+
+        public virtual ActionResult Edit(Guid id)
+        {
+            var copy = _copyR.Get(id);
+            copy.Volumes = _volumeR.GetByCopyIDAsList(id);
+            LoadParentStuff(copy.TitleID);
+
+            return View(copy);
+        }
+
+        [HttpPost]
+        public virtual ActionResult Edit(Guid id, Copy copy)
+        {
+            try
+            {
+                _copyR.Update(copy);
+
+                return RedirectToAction(MVC.Copy.Index());
+            }
+            catch
+            {
+                return View(copy);
+            }
+        }
+
+        public virtual ActionResult Delete(Guid id)
+        {
+            var copy = _copyR.Get(id);
+            LoadParentStuff(copy.TitleID);
+
+            return View(copy);
+        }
+
+        [HttpPost]
+        public virtual ActionResult Delete(Guid id, bool actuallyRemove)
+        {
+            try
+            {
+                if (actuallyRemove)
+                    _copyR.Delete(id);
+                else
+                    _copyR.Remove(id);
+
+                return RedirectToAction(MVC.Copy.Index());
+            }
+            catch
+            {
+                var Copy = _copyR.Get(id);
+                if (Copy != null)
+                    return View(Copy);
+                else
+                    return RedirectToAction(MVC.Copy.Index());
+            }
+        }
+    }
+}
---
+++ BookItAdmin/Controllers/VolumeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FbcBookIt.Repository;
+using FbcBookIt.Entity;
+
+namespace BookItAdmin.Controllers
+{
+    public partial class VolumeController : Controller
+    {
+        private ITitleR _titleR = null;
+        private ICopyR _copyR = null;
+        private IVolumeR _volumeR = null;
+        public VolumeController(ITitleR titleR, ICopyR copyR, IVolumeR volumeR)
+        {
+            if (titleR == null)
+                throw new ArgumentNullException("Title repository cannot be null");
+            if (copyR == null)
+                throw new ArgumentNullException("Copy repository cannot be null");
+            if (volumeR == null)
+                throw new ArgumentNullException("Volume repository cannot be null");
+
+            _titleR = titleR;
+            _copyR = copyR;
+            _volumeR = volumeR;
+        }
+
+        public virtual ActionResult Index(Guid id)
+        {
+            var volume = _volumeR.Get(id);
+            if (volume.CopyID.HasValue)
+                LoadParentStuff(volume.CopyID.Value);
+
+            return View(volume);
+        }
+
+        private void LoadParentStuff(Guid copyId)
+        {
+            var copy = _copyR.Get(copyId);
+            var title = _titleR.Get(copy.TitleID);
+            ViewBag.AccessionNumber = copy.AccessionNumber;
+            ViewBag.CopyId = copy.CopyId;
+            ViewBag.TitleName = title.Name;
+            ViewBag.TitleId = title.TitleID;
+        }
+
+        public virtual ActionResult Details(Guid id)
+        {
+            var volume = _volumeR.Get(id);
+            if (volume.CopyID.HasValue)
+                LoadParentStuff(volume.CopyID.Value);
+            return View(volume);
+        }
+
+        public virtual ActionResult Create(Guid copyId)
+        {
+            var volume = new Volume();
+            volume.CopyID = copyId;
+            LoadParentStuff(copyId);
+            return View(volume);
+        }
+
+        [HttpPost]
+        public virtual ActionResult Create(Volume volume)
+        {
+            try
+            {
+                var id = _volumeR.InsertAndReturnPrimaryKey(volume);
+
+                return RedirectToAction(MVC.Volume.Index(id));
+            }
+            catch
+            {
+                return View(volume);
+            }
+        }
+
+        public virtual ActionResult Edit(Guid id)
+        {
+            var volume = _volumeR.Get(id);
+            if (volume.CopyID.HasValue)
+                LoadParentStuff(volume.CopyID.Value);
+            return View(volume);
+        }
+
+        [HttpPost]
+        public virtual ActionResult Edit(Guid id, Volume volume)
+        {
+            try
+            {
+                _volumeR.Update(volume);
+
+                return RedirectToAction(MVC.Volume.Index(volume.VolumeID));
+            }
+            catch
+            {
+                return View(volume);
+            }
+        }
+
+        public virtual ActionResult Delete(Guid id)
+        {
+            var volume = _volumeR.Get(id);
+            if (volume.CopyID.HasValue)
+                LoadParentStuff(volume.CopyID.Value);
+            return View(volume);
+        }
+
+        [HttpPost]
+        public virtual ActionResult Delete(Guid id, bool actuallyRemove)
+        {
+            try
+            {
+                var volume = _volumeR.Get(id);
+                var copyId = volume.CopyID;
+
+                _volumeR.Delete(id);
+
+                if (copyId.HasValue)
+                {
+                    return RedirectToAction(MVC.Copy.Details(copyId.Value));
+                }
+                return RedirectToAction(MVC.Copy.Index());
+            }
+            catch
+            {
+                var volume = _volumeR.Get(id);
+                if (volume != null)
+                    return View(volume);
+                else
+                    return RedirectToAction(MVC.Copy.Index());
+            }
+        }
+    }
+}
